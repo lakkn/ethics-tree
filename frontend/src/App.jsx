0de8380c02e43dc947d183e628169b6bb711a482
Lakshay Kansal <laksh.kansal@gmail.com>
2025-04-27 19:32:30 -0400

    wrote code for ethics tree

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import EthicsTree from './EthicsTree'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <EthicsTree />
+    </div>
+  );
+}
